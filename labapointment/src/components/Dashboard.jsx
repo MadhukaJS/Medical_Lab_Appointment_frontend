603dacc1ab0df3cc764a7efba83112e02e3c4e65
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2024-03-11 15:58:01 +0530

    design fix

--- labapointment/src/components/Dashboard.jsx
+++ labapointment/src/components/Dashboard.jsx
@@ -1,132 +1,107 @@
-import React from 'react'
+import React from 'react';
 import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
-import './style.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './style.css'; 
 
 export default function Dashboard() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-dark">
+      {/* Navbar */}
+      <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: '#00264d'}}>
         <div className="container">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="/">
             <img
-              id="mps-logo"
+              id="medilab-logo"
               src=""
               alt="Medi lab"
               draggable="false"
               height="30"
             />
-           
           </a>
           <button
             className="navbar-toggler"
             type="button"
-            data-mdb-toggle="collapse"
-            data-mdb-target="#navbarSupportedContent"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <i className="fas fa-bars"></i>
+            <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            
-            <form className="d-flex align-items-center w-75 form-search">
-              <div className="input-group">
-                
-                <input
-                  type="search"
-                  className="form-control"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-              </div>
-              <a href="#!" className="text-white">
-                <i className="fas fa-search ps-3"></i>
-              </a>
+            <form className="form-inline my-2 my-lg-0 mx-auto">
+              
             </form>
 
-            <ul className="navbar-nav ms-3">
-              {/* <li className="nav-item" style={{ width: "65px" }}> */}
-                <a className="nav-link d-flex align-items-center" href="/login">
-                  <button>Log In</button>
-                </a>
-                
-                <a className="nav-link d-flex align-items-center" href="/registration">
-                <button>Sign up</button>
-                </a>
-              {/* </li> */}
+
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/login">Log In</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/registration">Sign Up</a>
+              </li>
             </ul>
-            
           </div>
         </div>
       </nav>
-      {/* <!-- Navbar --> */}
-      {/* ----------------- */}
+      {/* End Navbar */}
 
-      <Carousel>
+      {/* Carousel */}
+      <Carousel showThumbs={false}>
         <div>
-          <img
-            src="https://www.mic-mainz.de/wp-content/uploads/2023/07/MIC_Webbanner_2000x700px.jpg"
-            alt="Image 1"
-          />
+          <img src="https://www.mic-mainz.de/wp-content/uploads/2023/07/MIC_Webbanner_2000x700px.jpg" alt="Image 1" />
+          <p className="legend">Welcome to Medilab! We are a state-of-the-art laboratory dedicated to providing accurate and reliable testing services. Our team of experienced professionals utilizes cutting-edge technology to deliver precise results for various industries and applications.</p>
         </div>
         <div>
-          <img
-            src="https://www.mic-mainz.de/wp-content/uploads/2023/07/MIC_Web-Banner_Lab_140623_2000x700.jpg"
-            alt="Image 1"
-          />
+          <img src="https://www.mic-mainz.de/wp-content/uploads/2023/07/MIC_Web-Banner_Lab_140623_2000x700.jpg" alt="Image 2" />
+          <p className="legend">Image 2</p>
         </div>
         <div>
-          <img
-            src="https://www.mic-mainz.de/wp-content/uploads/2023/07/MIC_Webbanner_2000x700px.jpg"
-            alt="Image 1"
-          />
+          <img src="https://www.mic-mainz.de/wp-content/uploads/2023/07/MIC_Webbanner_2000x700px.jpg" alt="Image 3" />
+          <p className="legend">Image 3</p>
         </div>
-
       </Carousel>
 
-        <div>
-          <center>
-        <h3>EXPLORE ADVERTISEMENTS</h3>
-        </center>
-        </div>
+      {/* Explore Advertisements */}
+      <div className="text-center mt-4">
+        <h3>Make your apointment</h3>
+      </div>
 
-      <div className="d-flex flex-wrap justify-content-center align-items-center p-4">
-        {/* Card 1 */}
-        <div className="col-md-3">
+      {/* Card Grid */}
+      <div className="row justify-content-center mt-4">
+        <div className="col-md-4 mb-4">
           <div className="card">
-            <img
-              src="https://img.freepik.com/free-photo/white-sport-sedan-with-colorful-tuning-road_114579-5044.jpg?w=996&t=st=1698294435~exp=1698295035~hmac=818ff762e044fd8dd9b9a237c615c6cdb90f3e201732838e08524094457d5d5e"
-              alt="Image 3"
-              className="card-img-top"
-            />
+            <img src="https://via.placeholder.com/350" className="card-img-top" alt="Car Image" />
             <div className="card-body">
-              <h5 className="card-title">car test1</h5>
-              <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur in ullam iusto! Itaque sequi repudiandae impedit. Libero ipsum architecto voluptate quibusdam dolore reprehenderit voluptates molestiae sapiente, quod non temporibus esse..</p>
-              <a href="/Addbookings" className="btn">
-              Book a time for view
-              </a>
+              <h5 className="card-title">Car Test 1</h5>
+              <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              {/* <a href="/Addbookings" className="btn btn-primary">Book a time for view</a> */}
             </div>
           </div>
         </div>
-
-
-
-    
-
-        {/* Footer */}
-        <footer className="sticky-footer ">
-          <div className="container my-auto">
-            <div className="copyright text-center my-auto">
-              <span>MPS motor Traders &copy;2023</span>
+        {/* Add more cards here */}
+        <div className="col-md-4 mb-4">
+          <div className="card">
+            <img src="https://www.southcoasthealth.com/assets/upload/a7c59a87-117c-407f-8c47-2718ff76baa4/doctor-testing-blood-sugar-for-diabetes.jpg" className="card-img-top" alt="Car Image" />
+            <div className="card-body">
+              <h5 className="card-title">Car Test 1</h5>
+              <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              {/* <a href="/Addbookings" className="btn btn-primary">Book a time for view</a> */}
             </div>
           </div>
-        </footer>
+        </div>
         
       </div>
+
+      {/* Footer */}
+      <footer className="sticky-footer bg-dark text-white py-3" >
+        <div className="container text-center" style={{backgroundColor: '#00264d'}}>
+          <span>Medi lab Medical laboratary &copy; 2023</span>
+        </div>
+      </footer>
     </div>
-  )
+  );
 }
